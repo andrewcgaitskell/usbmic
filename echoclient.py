cc31a58f03a91c79712b9677fa8d780278ef97dd
andrewcgaitskell <andrew@gaitskell.com>
2020-06-29 15:21:26 +0100

    Update echoclient.py

--- echoclient.py
+++ echoclient.py
@@ -4,7 +4,9 @@ HOST = '192.168.1.8'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
+hellomessage = 'Hello, world'
+hello_as_bytes = str.encode(hellomessage)
+s.sendall(hello_as_bytes)
 data = s.recv(1024)
 s.close()
 print('Received', repr(data))
