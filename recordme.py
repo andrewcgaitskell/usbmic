ee284d21be8e368cdd269b79fc02448151f3214d
andrewcgaitskell <andrew@gaitskell.com>
2020-06-29 10:00:09 +0100

    Update recordme.py

--- recordme.py
+++ recordme.py
@@ -2,7 +2,7 @@ import pyaudio
 import wave
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
-chans = 1 # 1 channel
+chans = 2 # 1 channel
 samp_rate = 44100 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
 record_secs = 3 # seconds to record
