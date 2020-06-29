00d2b0d419ccfc55be627cb2b23e3b99962ab2d6
andrewcgaitskell <andrew@gaitskell.com>
2020-06-29 14:52:52 +0100

    Create talk.py

---
+++ talk.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP = "192.168.1.9"
+UDP_PORT = 1234
+MESSAGE = b"Hello, World!"
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
