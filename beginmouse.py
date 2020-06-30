dfd15b3b4381028fb8088be3da86b9c65382b4b3
andrewcgaitskell <andrew@gaitskell.com>
2020-06-30 12:03:26 +0100

    Update beginmouse.py

--- beginmouse.py
+++ beginmouse.py
@@ -10,26 +10,33 @@ dev = usb.core.find(idVendor=0x0c76, idProduct=0x1529)
 # or, uncomment the next line to search instead by the hexidecimal equivalent
 #dev = usb.core.find(idVendor=0x45e, idProduct=0x77d)
 # first endpoint
-interface = 0
-endpoint = dev[0][(0,0)][0]
+endpoint = dev[0].interfaces()[0].endpoints()[0]
+interface=dev[0].interfaces()[0].bInterfaceNumber
+dev.reset()
 # if the OS kernel already claimed the device, which is most likely true
 # thanks to http://stackoverflow.com/questions/8218683/pyusb-cannot-set-configuration
-if dev.is_kernel_driver_active(interface) is True:
+if dev.is_kernel_driver_active():
   # tell the kernel to detach
   dev.detach_kernel_driver(interface)
   # claim the device
-  usb.util.claim_interface(dev, interface)
-collected = 0
-attempts = 50
-while collected < attempts :
-    try:
-        data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
-        collected += 1
-        print(data)
-    except usb.core.USBError as e:
-        data = None
-        if e.args == ('Operation timed out',):
-            continue
+  # usb.util.claim_interface(dev, interface)
+
+dev.set_configuration()
+eaddr = ep.bEndpointAddress
+
+r=dev.read(eaddr,1024)
+print(len(r))  
+#collected = 0
+#attempts = 50
+#while collected < attempts :
+#    try:
+#        data = dev.read(endpoint.bEndpointAddress,endpoint.wMaxPacketSize)
+#        collected += 1
+#        print(data)
+#    except usb.core.USBError as e:
+#        data = None
+#        if e.args == ('Operation timed out',):
+#            continue
 # release the device
 usb.util.release_interface(dev, interface)
 # reattach the device to the OS kernel
