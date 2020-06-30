46ad8a042b84b8f44375a1ec0b1566095537d5fa
andrewcgaitskell <andrew@gaitskell.com>
2020-06-30 18:53:43 +0100

    Update simpleclaim.py

--- simpleclaim.py
+++ simpleclaim.py
@@ -7,22 +7,22 @@ dev = usb.core.find(idVendor=0x0c76, idProduct=0x1529)
 def main():
 
         if dev is None:
-            print "device not found"
+            print("device not found")
 
         else:
-        print "device found"
+        print("device found")
         if dev.is_kernel_driver_active(interface) is True:
-            print "but we need to detach kernel driver"
+            print("but we need to detach kernel driver")
             dev.detach_kernel_driver(interface)
-            print "claiming device"
+            print("claiming device")
             usb.util.claim_interface(dev, interface)
 
 
-            print "release claimed interface"
+            print("release claimed interface")
             usb.util.release_interface(dev, interface)
-            print "now attaching the kernel driver again"
+            print("now attaching the kernel driver again")
             dev.attach_kernel_driver(interface)
-            print "all done"
+            print("all done")
 return 0
 
 if __name__ == '__main__':
