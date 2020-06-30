c4fdeca7e9c7dbfdee82b07602ff0fc2424be53b
andrewcgaitskell <andrew@gaitskell.com>
2020-06-30 09:52:27 +0100

    Update closesockets.py

--- closesockets.py
+++ closesockets.py
@@ -2,5 +2,8 @@ import socket
 import select
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('', 50007))
+try:
+  serversocket.bind(('', 50007))
+except:
+  pass
 serversocket.close()
