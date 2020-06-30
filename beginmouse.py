b3702982c1bede9662ef920a7b4470a8344afe39
andrewcgaitskell <andrew@gaitskell.com>
2020-06-30 12:10:26 +0100

    Update beginmouse.py

--- beginmouse.py
+++ beginmouse.py
@@ -11,8 +11,8 @@ print(dev)
 # or, uncomment the next line to search instead by the hexidecimal equivalent
 #dev = usb.core.find(idVendor=0x45e, idProduct=0x77d)
 # first endpoint
-endpoint = dev[0].interfaces()[0].endpoints()[0]
-interface=dev[0].interfaces()[0].bInterfaceNumber
+endpoint = dev[0].interfaces()[1].endpoints()[0]
+interface= dev[0].interfaces()[1].bInterfaceNumber
 dev.reset()
 # if the OS kernel already claimed the device, which is most likely true
 # thanks to http://stackoverflow.com/questions/8218683/pyusb-cannot-set-configuration
