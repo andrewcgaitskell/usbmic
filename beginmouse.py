5cd18b269583afe2dad714d749cae962f8995e15
andrewcgaitskell <andrew@gaitskell.com>
2020-06-30 14:41:29 +0100

    Update beginmouse.py

--- beginmouse.py
+++ beginmouse.py
@@ -30,7 +30,7 @@ if dev.is_kernel_driver_active(interface):
 #dev.set_configuration()
 eaddr = endpoint.bEndpointAddress
 
-r = dev.read(eaddr, buffer)
+r = dev.read(eaddr, 200)
 print(len(r))  
 #collected = 0
 #attempts = 50
