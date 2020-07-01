646bcfd906b09d46c19a439989cc6c721c6d21b1
andrewcgaitskell <andrew@gaitskell.com>
2020-07-01 11:59:48 +0100

    Update connect2mic.c

--- connect2mic.c
+++ connect2mic.c
@@ -56,7 +56,7 @@ int main(int argc, char*argv[])
 
   /* Claim interface #0. */
   /*res = libusb_claim_interface(handle, 0);*/
-  res = libusb_claim_interface(handle, (1,1));
+  res = libusb_claim_interface(handle,1,1);
   if (res != 0)
   {
     fprintf(stderr, "Error claiming interface.\n");
